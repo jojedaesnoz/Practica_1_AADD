f5d74698859be9806a355d3277f6021d8c6ba0d6
jojedaesnoz <jojedaesnoz@gmail.com>
2018-11-15 18:57:51 +0100

    Vista basica completa

--- src/com/company/ui/Vista.java
+++ src/com/company/ui/Vista.java
@@ -3,9 +3,7 @@ package com.company.ui;
 import com.company.base.Pelicula;
 
 import javax.swing.*;
-import javax.swing.event.MouseInputListener;
 import java.awt.*;
-import java.awt.event.MouseEvent;
 
 public class Vista extends JFrame {
     private JPanel panelInformacion, panelBotones;
@@ -13,79 +11,118 @@ public class Vista extends JFrame {
     private DefaultListModel<Pelicula> modeloPeliculas;
     private JList<Pelicula> listaPeliculas;
     private JButton btNuevo, btGuardar, btModificar, btCancelar, btEliminar;
-    private JTextField tfTitulo, tfSinopsis, tfValoracion, tfRecaudacion;
+    private JTextField tfTitulo, tfValoracion, tfRecaudacion;
+    private JTextArea taSinopsis;
     private JLabel lImagen;
 
     public Vista() {
-        prepararComponentes();
+        inicializarComponentes();
+        colocarComponentes();
+        crearPanelBotones();
         prepararVentana();
     }
 
-    private void prepararComponentes(){
-        setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
+    private void inicializarComponentes() {
+        tfTitulo = new JTextField();
+        taSinopsis = new JTextArea();
+        tfValoracion = new JTextField();
+        tfRecaudacion = new JTextField();
+        lImagen = new JLabel("IMAGEN");
 
         btNuevo = new JButton("Nuevo");
         btGuardar = new JButton("Guardar");
         btModificar = new JButton("Modificar");
         btCancelar = new JButton("Cancelar");
         btEliminar = new JButton("Eliminar");
+    }
 
-        tfTitulo = new JTextField();
-        tfSinopsis = new JTextField();
-        tfValoracion = new JTextField();
-        tfRecaudacion = new JTextField();
-        lImagen = new JLabel();        // TODO: poner imagen por defecto (pulsa para guardar una imagen)
+    private void colocarComponentes(){
+        JPanel contenedor = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
 
+        // COLOCAR
 
-        // Colocar con GridBagLayout
+        JLabel lTitulo = new JLabel("Título", SwingConstants.CENTER);
+        JLabel lSinopsis = new JLabel("Sinopsis", SwingConstants.CENTER);
+        JLabel lValoracion = new JLabel("Valoración", SwingConstants.CENTER);
+        JLabel lRecaudacion = new JLabel("Recaudación", SwingConstants.CENTER);
+
+        // DAR TAMS Y BORDES
+        for (JLabel label: new JLabel[]{lTitulo, lSinopsis, lValoracion, lRecaudacion}){
+            label.setPreferredSize(new Dimension(80, 30));
+        }
+        for (JComponent entrada: new JComponent[]{tfTitulo, tfValoracion, tfRecaudacion}) {
+            entrada.setPreferredSize(new Dimension(150, 25));
+            entrada.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        }
+        taSinopsis.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+        // IMAGEN
+        lImagen.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
-        gbc.gridheight = 2;
         gbc.fill = GridBagConstraints.BOTH;
-        add(lImagen, gbc);
-
+        contenedor.add(lImagen, gbc);
         gbc.gridwidth = 1;
-        gbc.gridheight = 1;
         gbc.fill = GridBagConstraints.NONE;
+
+        // ETIQUETAS
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        contenedor.add(lTitulo, gbc);
+
+        gbc.gridy++;
+        gbc.anchor = GridBagConstraints.NORTH;
+        contenedor.add(lSinopsis, gbc);
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        gbc.gridy++;
+        contenedor.add(lValoracion, gbc);
+
+        gbc.gridy++;
+        contenedor.add(lRecaudacion, gbc);
+
+        // Poner entradas
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        contenedor.add(tfTitulo, gbc);
+
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridy++;
+        contenedor.add(taSinopsis, gbc);
+        gbc.weighty = 0;
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridy++;
+        contenedor.add(tfValoracion, gbc);
+
+        gbc.gridy++;
+        contenedor.add(tfRecaudacion, gbc);
+
+        // LISTA
+        add(contenedor);
+        JScrollPane panelPeliculas = crearPanelPeliculas();
+        gbc.weightx = 1;
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.gridheight = 5;
+        gbc.fill = GridBagConstraints.BOTH;
+        contenedor.add(panelPeliculas, gbc);
     }
 
     private void prepararVentana() {
-        setSize(600, 400);
+        setSize(550, 300);
+        setMinimumSize(new Dimension(550, 300));
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    private JPanel panelEntrada(String nombre, JTextField textField) {
-        JPanel panel = new JPanel();
-        panel.add(new JLabel(nombre));
-        panel.add(textField);
-        return panel;
-    }
+    private void crearPanelBotones() {
 
-    private JPanel crearPanelInformacion() {
-        // GridLayout con hueco para las entradas y la imagen
-        JTextField[] entradas = {tfTitulo, tfSinopsis, tfValoracion, tfRecaudacion};
-        JPanel contenedor = new JPanel(new GridLayout(entradas.length + 1, 1, 10, 10));
-
-        //
-        contenedor.add(lImagen);
-        for (JTextField textField: entradas) {
-            textField = new JTextField();
-            contenedor.add(textField);
-        }
-
-        // Grid con hueco para meter contenedor y margenes
-        JPanel panelInfo = new JPanel(new GridLayout(1, 1));
-        Dimension margen = new Dimension(30, 30);
-        panelInfo.add(Box.createRigidArea(margen));
-        panelInfo.add(contenedor);
-        panelInfo.add(Box.createRigidArea(margen));
-        return panelInfo;
-    }
-
-    private JPanel crearPanelBotones() {
         JPanel botonera = new JPanel();
         JButton[] botones = {btNuevo, btGuardar, btModificar, btCancelar, btEliminar};
 
@@ -93,7 +130,7 @@ public class Vista extends JFrame {
         for (JButton boton: botones)
             botonera.add(boton);
 
-        return botonera;
+        add(botonera, BorderLayout.SOUTH);
     }
 
     private JScrollPane crearPanelPeliculas() {
